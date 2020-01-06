f90ee7cd8cd6e342dfa9c7a9783650755e71fe41
John Rogala <johnrossrogala@gmail.com>
2020-01-06 23:45:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
-    @post = post.find(params[:id])
+    @post = Post.find(params[:id])
     render json: @post
   end
 
