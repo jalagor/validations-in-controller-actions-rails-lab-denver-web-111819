a7c570596c7accfd6f7abbcb48dc25cbaaab053e
John Rogala <johnrossrogala@gmail.com>
2020-01-06 23:44:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
-    post = post.new
+    post = post.find(params[:id])
   end
 
   def edit
